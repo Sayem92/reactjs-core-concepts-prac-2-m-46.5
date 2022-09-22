c8666222909b2e728da807dfa0c4ca62b7b6b548
Sayem92 <sayemmiha123@gmail.com>
2022-09-22 12:59:02 +0600

    add design

--- src/conponent/Cocktails/Cocktail.js
+++ src/conponent/Cocktails/Cocktail.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./Cocktail.css"
 
 const Cocktail = (props) => {
     const {strDrinkThumb,strIngredient3,strInstructions,strGlass} = props.cocktail
     return (
-        <div>
+        <div className='cocktail'>
             <img src={strDrinkThumb}alt="" />
             <h3>Name: {strIngredient3}</h3>
             <h5>Quality: {strGlass}</h5>
